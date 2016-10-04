b97c87749f97c4614efa005302e59e6ed6d7413f
Thành Vũ <vuthanh20286@gmail.com>
2016-10-04 23:09:31 +0700

    First commit source code

---
+++ Frontend-Website/Frontend-SercuIntegraded/GenerateModels/NewsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Frontend_SercuIntegraded.GenerateModels;
+using System.Configuration;
+
+namespace Frontend_SercuIntegraded.GenerateModels
+{
+    public sealed class NewsDbContext : FrontendDbContext
+    {
+        public NewsDbContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
